6861f3a7fe6dfb7f99fc9d53a216ca4180afb23d
tryndadi <trindade.vt@outlook.com>
2026-03-06 15:38:36 -0300

    fix vercel routes

--- api/server.js
+++ api/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const pool = require("../db");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/transacoes", (req, res) => {
+    res.json([]);
+});
 
 app.get("/api/categorias/:tipo", async (req, res) => {
 
