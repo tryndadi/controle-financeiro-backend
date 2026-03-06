dbf789575753875e1b0304f29c91415333b25b78
tryndadi <trindade.vt@outlook.com>
2026-03-06 14:23:06 -0300

    fix static files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, ".")));
 
 app.get("/categorias/:tipo", async (req, res) => {
 
