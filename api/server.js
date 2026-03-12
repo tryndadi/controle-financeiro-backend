3a2a4c93a3730eefde63823691f654a5a8200326
tryndadi <trindade.vt@outlook.com>
2026-03-12 16:41:19 -0300

    fix: logo

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("../public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
